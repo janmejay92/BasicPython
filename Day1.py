ce30bb8f7174756ab555ac9e5eb80db1674d5b61
janmejay kumar <janmejayrai79@gmail.com>
2026-06-22 14:46:02 +0530

    day 1 calculator

--- Day1.py
+++ Day1.py
@@ -1,2 +1,25 @@
 print("Hello, world!")
 print("my name is janmejay rai")
+print(" today is 1st day of python programming")
+print("Python is useful")
+# task 2 is print number
+print(25)
+# /printing string and number together
+print("hello my name is janmejay kumar ", "age: ",22)
+#  defining variable in python
+name="Janmejay kumar"
+age=22
+# print(name,age)
+print("Name", name)
+print("Age", age)
+# name=input("what is your name")
+# print("my name is:",name)
+# simple calculation
+a=int(input("enter you first number"))
+b= int(input("enter your second number"))
+c=(a+b)
+c1=a*b
+c2=a**2
+print(c)
+print(c1)
+print(c2)
---
+++ day1_tea_shop_calculator.py
@@ -0,0 +1,9 @@
+customer_name=input("what is your name")
+cups=int(input("how many cup of tea ?"))
+price_per_cups=10
+total_bill=cups*price_per_cups
+print("total bill is: ", total_bill)
+print("Thanks for visit",customer_name)
+
+
+
